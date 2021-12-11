535676e4c5b89e1c933846c646e646310ff96e8e
xt0r3 <kgs37@cam.ac.uk>
2021-12-11 13:08:59 +0100

    Commit #20: Added day-11

---
+++ day-11/buildCalculator.js
@@ -0,0 +1,25 @@
+const calculator = document.getElementsByClassName('calculator')[0];
+
+function addButton(content){
+    let div = document.createElement('DIV');
+    div.textContent = content;
+    div.classList.add('button');
+    calculator.appendChild(div);
+}
+
+for(let i = 1; i < 10; i++){
+    addButton(i.toString());
+}
+
+addButton('-');
+addButton('0');
+addButton('+');
+addButton('÷');
+
+let div = document.createElement('DIV');
+div.textContent = 'CE';
+div.id = 'ce';
+div.classList.add('button');
+calculator.appendChild(div);
+
+addButton('×');
