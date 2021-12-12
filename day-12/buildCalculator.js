21a7d5bb792c27ad017f368797748405e3bcd33e
xt0r3 <kgs37@cam.ac.uk>
2021-12-12 14:40:20 +0100

    Commit #25: UPDATED day-12

--- day-12/buildCalculator.js
+++ day-12/buildCalculator.js
@@ -14,21 +14,23 @@ let signs = {
 }
 let active = displays.ONE;
 
-function addToActive(content){
-    if(active.textContent !== "" || content !== "0"){
+function addToActive(content) {
+    if (active.textContent !== "" || content !== "0") {
+        active.textContent += content;
+    } else if (active === displays.ONE || displays.SIGN.textContent !== signs.DIVISION && active.textContent === "") {
         active.textContent += content;
     }
 }
 
-function resetDisplays(){
-    for(let display in displays){
+function resetDisplays() {
+    for (let display in displays) {
         displays[display].textContent = "";
     }
     active = displays.ONE;
 }
 
-function changeSign(content){
-    if(displays.ANSWER.textContent !== "ERROR" && displays.ONE.textContent !== ""){
+function changeSign(content) {
+    if (displays.ANSWER.textContent !== "ERROR" && (displays.ONE.textContent !== "" || content === signs.MINUS || displays.TWO.textContent !== "")) {
         displays.ONE.textContent = displays.ANSWER.textContent;
         displays.TWO.textContent = "";
         displays.ANSWER.textContent = "";
@@ -37,74 +39,97 @@ function changeSign(content){
     }
 }
 
+function parse(display) {
+    if (display.textContent === "") {
+        return 0;
+    } else {
+        let val = parseInt(display.textContent);
+        display.textContent = val.toString();
+        return val;
+    }
+}
 
-function calculateAnswer(){
+function processKey(key) {
+    if (key === '+') {
+        return signs.PLUS;
+    } else if (key === '-') {
+        return signs.MINUS;
+    } else if (key === '*') {
+        return signs.TIMES;
+    } else if (key === '/') {
+        return signs.DIVISION;
+    } else {
+        return key;
+    }
+}
+
+
+function calculateAnswer() {
 
     // CHECK FOR STRINGS OF DIGITS
-    String.prototype.isNumber = function(){return this.length === 0 || /^\d+$/.test(this);}
-    if(!displays.ONE.textContent.isNumber() || !displays.TWO.textContent.isNumber()){
+    String.prototype.isNumber = function () {
+        return this.length === 0 || /^\d+$/.test(this) || (this.charAt(0) === '-' && /^\d+$/.test(this.substring(1)));
+    }
+    if (!displays.ONE.textContent.isNumber() || !displays.TWO.textContent.isNumber()) {
         displays.ANSWER.textContent = "ERROR";
         return;
     }
 
     let sign = displays.SIGN.textContent;
     let ans = displays.ANSWER;
-    let first = parseInt(displays.ONE.textContent === "" ? "0" : displays.ONE.textContent);
-    let second = parseInt(displays.TWO.textContent === "" ? "0" : displays.TWO.textContent);
+
+    let first = parse(displays.ONE);
+    let second = parse(displays.TWO);
 
     // ADDITION
-    if(sign === signs.PLUS){
+    if (sign === signs.PLUS) {
         ans.textContent = (first + second).toString();
     }
     // SUBTRACTION
-    else if(sign === signs.MINUS){
+    else if (sign === signs.MINUS) {
         ans.textContent = (first - second).toString();
     }
     // MULTIPLICATION
-    else if(sign === signs.TIMES){
+    else if (sign === signs.TIMES) {
         ans.textContent = (displays.TWO.textContent === "" ? first : (first * second)).toString();
     }
     // DIVISION
-    else if(sign === signs.DIVISION){
-        if(displays.TWO.textContent === ""){
+    else if (sign === signs.DIVISION) {
+        if (displays.TWO.textContent === "") {
             ans.textContent = first.toString();
-        }
-        else if(second === 0){
+        } else if (second === 0) {
             ans.textContent = "ERROR";
-        }
-        else{
+        } else {
             ans.textContent = (first / second).toString();
         }
     }
     // EMPTY SIGN
-    else if(sign === signs.EMPTY && !isNaN(first)){
+    else if (sign === signs.EMPTY && !isNaN(first)) {
         ans.textContent = first.toString();
     }
     // ANYTHING ELSE
-    else{
+    else {
         ans.textContent = "ERROR";
     }
 }
 
-function addButton(content){
+function addButton(content) {
     let div = document.createElement('DIV');
     div.textContent = content;
     div.classList.add('button');
-    if(!isNaN(parseInt(content))){
+    if (!isNaN(parseInt(content))) {
         div.addEventListener('click', () => addToActive(content));
-    }
-    else if(content === "CE"){
+    } else if (content === "CE") {
         div.id = "ce";
         div.addEventListener('click', resetDisplays);
-    }
-    else{
+    } else {
         div.addEventListener('click', () => changeSign(content));
     }
     calculator.appendChild(div);
     return div;
 }
 
-for(let i = 1; i < 10; i++){
+for (let i = 1; i < 10; i++) {
     addButton(i.toString());
 }
 
@@ -116,3 +141,24 @@ addButton("CE");
 addButton(signs.TIMES);
 
 calculator.addEventListener('click', calculateAnswer);
+document.addEventListener('keydown', function (e) {
+    const buttons = document.getElementsByClassName('button');
+    console.log(e.key);
+    for (let i = 0; i < buttons.length; i++) {
+        let button = buttons[i];
+        let content = button.textContent;
+        if (processKey(e.key) === content) {
+            if (!isNaN(parseInt(content))) {
+                addToActive(content);
+            } else {
+                changeSign(content);
+            }
+        }
+    }
+    if (e.key === 'Backspace') {
+        if (active.textContent.length > 0) {
+            active.textContent = active.textContent.slice(0, -1);
+        }
+    }
+    calculateAnswer();
+});
